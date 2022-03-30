4032f4cb517419439e501cff0bba2f86c653b27f
xucong <xuerzong@gmail.com>
2022-03-30 14:56:58 +0800

    chore: add ga

---
+++ src/components/common/google-analytics.tsx
@@ -0,0 +1,20 @@
+export const GoogleAnalytics = () => {
+  return (
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-K73J0X0TZ2"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K73J0X0TZ2');
+        `,
+        }}
+      />
+    </>
+  );
+};
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { GoogleAnalytics } from '@/components/common/google-analytics';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="zh">
-        <Head />
+        <Head>
+          <GoogleAnalytics />
+        </Head>
         <body className="bg-white dark:bg-gray-900 dark:text-gray-300 transition-all">
           <Main />
           <NextScript />
