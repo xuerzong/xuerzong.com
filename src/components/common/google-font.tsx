a8fac913adeb6aba6479af4153e13ec0c48df06e
xuerzong <xuerzong@gmail.com>
2022-05-15 19:31:56 +0800

    fix: markdown rewrite

---
+++ src/components/common/google-font.tsx
@@ -0,0 +1,14 @@
+const GoogleFontLinks = () => {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="*" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Recursive:wght,CASL,MONO@300..700,1,1&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
+};
+
+export default GoogleFontLinks;
