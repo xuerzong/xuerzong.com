59f81c10d54b28c841ef23189b6990a61b1bd504
xuerzong <xuerzong@gmail.com>
2022-08-02 00:20:14 +0800

    feat: v1.3.0
    fix: package.json

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
 }
 
 const Container: React.FC<ContainerProps> = ({ children, className }) => (
-  <div className={cls('max-w-screen-lg mx-auto px-4 lg:px-0', className)}>{children}</div>
+  <div className={cls('max-w-screen-md mx-auto px-4 lg:px-0', className)}>{children}</div>
 )
 
 export default Container
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,66 @@
+import { Bike, Camera, DeviceGamepad2, Terminal2 } from 'tabler-icons-react'
+import cls from 'classnames'
+
+const hobbiesIconClassName = 'w-8 h-8'
+
+const hobbies = [
+  {
+    key: 'sport',
+    title: '骑行',
+    icon: <Bike className={hobbiesIconClassName} />,
+    className: 'text-emerald-600 dark:text-emerald-400 bg-emerald-600',
+  },
+  {
+    key: 'code',
+    title: '编程',
+    icon: <Terminal2 className={hobbiesIconClassName} />,
+    className: 'text-sky-600 dark:text-sky-400 bg-sky-600',
+  },
+  {
+    key: 'photo',
+    title: '摄影',
+    icon: <Camera className={hobbiesIconClassName} />,
+    className: 'text-violet-600 dark:text-violet-400 bg-violet-600',
+  },
+  {
+    key: 'game',
+    title: '游戏',
+    icon: <DeviceGamepad2 className={hobbiesIconClassName} />,
+    className: 'text-rose-600 dark:text-rose-400 bg-rose-600',
+  },
+]
+
 const Hero: React.FC = () => {
+  const hobbiesRender = hobbies.map(({ key, title, icon, className }) => {
+    return (
+      <li
+        key={key}
+        title={title}
+        className={cls(
+          'flex flex-wrap items-center justify-center bg-opacity-10 hover:bg-opacity-20 py-2 ring-4 ring-current rounded whitespace-nowrap cursor-pointer',
+          className
+        )}
+      >
+        <span className="mr-2">{icon}</span>
+        <div className="inline-block text-xl">{title}</div>
+      </li>
+    )
+  })
+
   return (
-    <section className="py-12">
-      <h2 className="text-4xl font-bold mb-4">我叫徐聪</h2>
-      <p>
-        一名前端开发工程，常用技术栈是
-        <code className="mx-2 text-primary-500">React + TS</code>
+    <section className="py-24 text-center">
+      <h2 className="text-7xl font-bold mb-6">
+        我叫
+        <span className="inline-block text-primary-600 dark:text-primary-400 border-b-4 border-current">
+          徐聪
+        </span>
+      </h2>
+
+      <p className="opacity-75 mt-6 mb-16">
+        一名99年有志青年，只希望我爱的人和爱我的人<strong>身体健康</strong>
       </p>
+
+      <ul className="grid grid-cols-2 md:grid-cols-4 gap-8">{hobbiesRender}</ul>
     </section>
   )
 }
