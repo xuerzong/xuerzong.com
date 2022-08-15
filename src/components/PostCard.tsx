1a42b33a17d84ad46b556d6fcc34c564205b5ec6
xuerzong <xuerzong@gmail.com>
2022-08-16 00:43:32 +0800

    fix: timeline of post card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,23 +1,22 @@
 import Link from 'next/link'
 import { Post } from 'contentlayer/generated'
 import Time from './Time'
-import Category from './Category'
 
 interface PostCardProps extends Post {}
 
 const PostCard: React.FC<PostCardProps> = ({ title, slug, description, date, category }) => (
-  <article className="select-none">
-    <Link href={`/posts/${slug}`} passHref>
-      <h1 className="inline-block text-lg my-2 font-bold decoration-4 hover:text-primary-500 cursor-pointer">
-        {title}
-      </h1>
-    </Link>
-
-    <p>{description}</p>
-
-    <div className="flex items-center justify-between py-4 text-sm text-gray-500">
-      <Category>{category}</Category>
+  <article className="flex flex-col md:flex-row pb-6 select-none">
+    <div className="basis-0 md:basis-40 flex-grow-0 flex-shrink-0 pt-2 mr-4 text-sm text-gray-500">
       <Time>{date}</Time>
+    </div>
+    <div>
+      <Link href={`/posts/${slug}`} passHref>
+        <h2 className="inline-block text-lg my-2 font-bold decoration-4 hover:text-primary-500 cursor-pointer">
+          {title}
+        </h2>
+      </Link>
+
+      <p>{description}</p>
     </div>
   </article>
 )
