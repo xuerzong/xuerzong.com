ca8f714e7282bbd8d8eaabc6d824a2203e8e2f1b
徐聪 <xuerzong@gmail.com>
2023-03-04 23:47:50 +0800

    Feature/v1.8.0 (#140)
    * feature/v1.8.0
    
    * v1.8.0

--- src/libs/remark/img-to-jsx.ts
+++ src/libs/remark/img-to-jsx.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import sizeOf from 'image-size'
 import { Literal, Node, Parent } from 'unist'
 import { visit } from 'unist-util-visit'
+import { getPlaiceholder } from 'plaiceholder'
 
 type ImageNode = Parent & {
   url: string
@@ -10,8 +11,30 @@ type ImageNode = Parent & {
   attributes: (Literal & { name: string })[]
 }
 
-export default function remarkImgToJsx() {
-  return (tree: Node) => {
+const addImageProps = async (imageNode: ImageNode) => {
+  // only local image
+  if (fs.existsSync(`${process.cwd()}/public${imageNode.url}`)) {
+    const dimensions = sizeOf(`${process.cwd()}/public${imageNode.url}`)
+    const { base64 } = await getPlaiceholder(imageNode.url)
+
+    // Convert original node to next/image
+    imageNode.type = 'mdxJsxFlowElement'
+    imageNode.name = 'Image'
+    imageNode.attributes = [
+      { type: 'mdxJsxAttribute', name: 'alt', value: imageNode.alt },
+      { type: 'mdxJsxAttribute', name: 'src', value: imageNode.url },
+      { type: 'mdxJsxAttribute', name: 'width', value: dimensions.width },
+      { type: 'mdxJsxAttribute', name: 'height', value: dimensions.height },
+      { type: 'mdxJsxAttribute', name: 'placeholder', value: 'blur' },
+      { type: 'mdxJsxAttribute', name: 'blurDataURL', value: base64 },
+    ]
+  }
+}
+
+export default function remarkImgTsoJsx() {
+  return async (tree: Node) => {
+    const imageNodes: ImageNode[] = []
+
     visit(
       tree,
       // only visit p tags that contain an img element
@@ -19,24 +42,16 @@ export default function remarkImgToJsx() {
         node.type === 'paragraph' && node.children.some((n) => n.type === 'image'),
       (node: Parent) => {
         const imageNode = node.children.find((n) => n.type === 'image') as ImageNode
-
-        // only local files
-        if (fs.existsSync(`${process.cwd()}/public${imageNode.url}`)) {
-          const dimensions = sizeOf(`${process.cwd()}/public${imageNode.url}`)
-
-          // Convert original node to next/image
-          ;(imageNode.type = 'mdxJsxFlowElement'),
-            (imageNode.name = 'Image'),
-            (imageNode.attributes = [
-              { type: 'mdxJsxAttribute', name: 'alt', value: imageNode.alt },
-              { type: 'mdxJsxAttribute', name: 'src', value: imageNode.url },
-              { type: 'mdxJsxAttribute', name: 'width', value: dimensions.width },
-              { type: 'mdxJsxAttribute', name: 'height', value: dimensions.height },
-            ])
-          node.type = 'div'
-          node.children = [imageNode]
+        if (imageNode) {
+          imageNodes.push(imageNode)
         }
+        node.type = 'div'
+        node.children = [imageNode]
       }
     )
+
+    for (const imageNode of imageNodes) {
+      await addImageProps(imageNode)
+    }
   }
 }
