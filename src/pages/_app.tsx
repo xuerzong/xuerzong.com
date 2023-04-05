b4b5cbc5a686202779b280cdc6cc5ef1e3396584
xucong <xuerzong@gmail.com>
2023-04-05 19:25:19 +0800

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from 'next-themes'
 import NProgress from 'nprogress'
 import type { ThemeProviderProps } from 'next-themes/dist/types'
 import AppLayout from '@/layouts/app'
+import GoogleAnalytics from '@/components/GoogleAnalytics'
 
 import 'nprogress/nprogress.css'
 import '@/styles/tailwind.css'
@@ -35,11 +36,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   }, [])
 
   return (
-    <ThemeProvider {...themeProviderProps}>
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics />
+      <ThemeProvider {...themeProviderProps}>
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ThemeProvider>
+    </>
   )
 }
 
